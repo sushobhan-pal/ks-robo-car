1959e8759eeacd13feee1cc273e3a50158b0250d
sushobhn-pal <sushobhan.pal@gmail.com>
2024-11-23 15:39:01 +0530

    Camera module integration

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,4 +1,5 @@
 import os
+import uuid;
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -15,10 +16,11 @@ def generate_launch_description():
 
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!! 
     package_name='ks-robo-car' #<--- CHANGE ME
-
+    
+    unique_entity_name = f"ks_bot_{uuid.uuid4()}"
+    
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
@@ -47,11 +49,12 @@ def generate_launch_description():
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
                     # launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
+             
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-entity', unique_entity_name],
                         output='screen')
 
 
